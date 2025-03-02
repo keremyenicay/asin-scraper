158408a1f3a224abb099c9833beaa691eef5763c
keremyenicay <keremyenicay026@gmail.com>
2025-03-02 23:37:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
     // Sayfa tamamen yüklendiğinde başlat
     window.onload = function() {
         console.log("✅ Sayfa yüklendi, script çalıştırılıyor.");
+
+    document.querySelectorAll("#kategoriSecimi, #filtreSec, #filtreKaydet").forEach(el => el.remove());
         initScript();
     };
 
